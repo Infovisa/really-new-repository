3779be8755fcdfc037e8738f0cde4163d8e79848
tstewar <tsteward@infovisa.com>
2015-03-18 11:10:55 -0600

    Added the line after

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
 		static void Main(string[] args)
 		{
             Console.WriteLine("Greetings mark!");
+			Console.WriteLine("The line after that one.");
 		}
 	}
 }

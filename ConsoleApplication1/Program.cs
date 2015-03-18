ce46711dc194c38f66db3478e4e64aef2c2d84f4
mrmetzner <mmetzner@infovisa.com>
2015-03-18 12:41:47 -0500

    I hope to commit to fork 2

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
 	{
 		static void Main(string[] args)
 		{
-            Console.WriteLine("Greetings mark! Eat this fork!");
+            Console.WriteLine("Greetings mark! FORK TWO!");
 		}
 	}
 }
